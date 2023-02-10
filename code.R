81547ad553fe438a35f39306e054304cb3d1f45d
htran <huong.tran@immunai.com>
2023-02-10 15:31:44 +0100

    another plot

--- code.R
+++ code.R
@@ -1,2 +1,12 @@
 library(tidyverse)
 ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy))
+# reorder is close to order, but is made to change the order of the factor levels.
+mpg$class = with(mpg, reorder(class, hwy, median))
+
+p <- mpg %>%
+  ggplot( aes(x=class, y=hwy, fill=class)) + 
+    geom_violin() +
+    xlab("class") +
+    theme(legend.position="none") +
+    xlab("")
+p
